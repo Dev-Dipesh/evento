dfd216099b23f5613bfa2675e23ec27bd1bed1f6
Dipesh Bhardwaj <itdeveloper03@gmail.com>
2024-04-19 11:49:34 +0530

    Added Vercel Speed Insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </Container>
+        <SpeedInsights />
       </body>
     </html>
   );
